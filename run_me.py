d5128b028a83834ff5036f7a7a4f4a67d0114b9b
eielsen <eielsen@fastmail.fm>
2025-02-06 11:17:33 +0100

    clean up

--- run_me.py
+++ run_me.py
@@ -55,15 +55,18 @@ from run_static_model_and_post_processing import run_static_model_and_post_proce
 
 #%% Configure DAC and test conditions
 
-METHOD_CHOICE = 6
+METHOD_CHOICE = 1
 match 3:
     case 1:
         FS_CHOICE = 4
-        DAC_CIRCUIT = 7
+        DAC_CIRCUIT = 7  # 6 bit spice
     case 2:
+        FS_CHOICE = 7
+        DAC_CIRCUIT = 7  # 6 bit spice
+    case 3:
         FS_CHOICE = 5
         DAC_CIRCUIT = 10
-    case 3:
+    case 4:
         FS_CHOICE = 5
         DAC_CIRCUIT = 11
 
