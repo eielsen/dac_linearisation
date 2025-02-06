367236632613f2c75216bb62811e93e7c38feef6
bikas3121 <bikas_adh@hotmail.com>
2025-02-06 17:49:44 +0100

    enob plots updates

--- utils/enob_gains_bar_chart.py
+++ utils/enob_gains_bar_chart.py
@@ -12,24 +12,24 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # %% DAC  and sampling frequency
-DAC_16bit_Fs_1MHz = 1
-DAC_6bit_Fs_1MHz = 2
-DAC_6bit_prop_Fs_65MHz = 3
-DAC_16bit_prop_Fs_65MHz = 4
-DAC_16bit_Fs_32MHz = 5
-DAC_6bit_Fs_32MHz = 6
+DAC_6bit_SKY_Fs_102MHz = 1
+DAC_6bit_SKY_Fs_327MHz = 2
+DAC_10bit_SKY_Fs_102MHz = 3
+DAC_10bit_SKY_Fs_327MHz = 4
+DAC_6bit_PRO_Fs_209MHz = 5
+DAC_10bit_PRO_Fs_209MHz = 6
 
 # %%
-match 1:
-    case 1: # 16-bit 1 MHz Trond
+match 5:
+    case 1: # 6 bit SkyWater at 1.02 Mhz 
         tech = 'SkyWater'
-        node = 'SKY130'
+        # node = 'SKY130'
         method0 = 'static'
-        method1 = 'SPICE'
+        method1 = 'spice'
 
         methods = f'{method0}-{method1}'
 
-        Nb = 16
+        Nb = 6
         Fs = 1.02
         static_baseline = 11.121
         static_physcal = 16.487
@@ -37,7 +37,7 @@ match 1:
         static_phfd = 11.283
         static_shpd = 10.750 
         static_dem = 8.645
-        #static_ilc= 15.551
+        static_mhoq= 15.551
 
         # Spice simulation results
         mos_model_baseline = 11.111
@@ -47,35 +47,33 @@ match 1:
         spice_phfd = 11.173
         spice_shpd = 10.672
         spice_dem = 8.640
-        #spice_ilc= 15.004
+        spice_mhoq= 15.004
 
-        # Calculate gains
-        #static_gains = np.array([static_physcal,  static_phfd, static_shpd, static_dem, static_nsdcal,static_ilc ])- static_baseline
-        #spice_gains = np.array([spice_physcal,  spice_phfd, spice_shpd, spice_dem, spice_nsdcal,spice_ilc])- spice_baseline
         
-        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem]) - static_baseline
-        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem]) - mos_model_baseline
+        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem, static_mhoq]) - static_baseline
+        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem, spice_mhoq]) - mos_model_baseline
         mos_model_gains = spice_gains
 
-        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM']
+        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM','MHOQ']
 
-    case 2: # 6-bit 1 MHz Trond
+    case 2: # 6-bit SkyWater at 32.07  
         tech = 'SkyWater'
-        node = 'SKY130'
+        # node = 'SKY130'
         method0 = 'static'
-        method1 = 'SPICE'
+        method1 = 'spice'
 
         methods = f'{method0}-{method1}'
 
         Nb = 6
-        Fs = 1.02
+        Fs = 32.07
 
         static_baseline = 6.051
         static_physcal = 6.832
         static_nsdcal = 8.022
         static_phfd = 6.752
         static_shpd= 3.538
-        static_dem = 4.924
+        static_dem = 4.924  
+        static_mhoq= 15.551
 
         # Spice simulation results
         mos_model_baseline = 6.093
@@ -85,150 +83,159 @@ match 1:
         spice_phfd = 6.783
         spice_shpd= 3.522
         spice_dem = 4.874
-
+        spice_mhoq = 1
         # Calculate gains
-        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem]) - static_baseline
-        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem]) - mos_model_baseline
+        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem, static_mhoq]) - static_baseline
+        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem, spice_mhoq]) - mos_model_baseline
         mos_model_gains = spice_gains
 
-        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM']
+        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM', 'MHOQ']
 
-    case 3: # SPECTRE 6 bit,
-        tech = 'proprietary'
-        node = '130 nm'
+    case 3: # 10 bit SkyWater at 1.02 Mhz 
+        tech = 'SkyWater'
+        # node = '130 nm'
         method0 = 'static'
-        method1 = 'Spectre'
+        method1 = 'spice'
 
         methods = f'{method0}-{method1}'
 
-        Nb = 6
-        Fs = 65.47
+        Nb = 10
+        Fs = 1.02
         static_baseline = 4.45
         static_physcal = 8.54
         static_nsdcal = 16.7
         static_phfd = 9.22
         static_shpd = 6.58
         static_dem = 8.06
+        static_mhoq= 15.551
 
-        mos_model_baseline = 5.37
-        spectre_baseline = mos_model_baseline
-        spectre_physcal = 5.64
-        spectre_nsdcal = 5.44
-        spectre_phfd = 9.19
-        spectre_shpd = 5.91
-        spectre_dem = 8.14
-        # spice_ilc_16_1= 14.646
-        # spice_mpc_16_1= 14.646
+
+        # Spice simulation results
+        mos_model_baseline = 6.093
+        spice_baseline = mos_model_baseline
+        spice_physcal = 6.164
+        spice_nsdcal = 8.343
+        spice_phfd = 6.783
+        spice_shpd= 3.522
+        spice_dem = 4.874
+        spice_mhoq = 1
 
         # Calculate gains
-        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem]) - static_baseline
-        spectre_gains = np.array([spectre_physcal, spectre_nsdcal, spectre_phfd, spectre_shpd, spectre_dem]) - mos_model_baseline
-        mos_model_gains = spectre_gains
+        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem, static_mhoq]) - static_baseline
+        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem, spice_mhoq]) - mos_model_baseline
+        mos_model_gains = spice_gains
 
-        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM']
+        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM', 'MHOQ']
 
-    case 4: # SPECTRE 16 bit,
-        tech = 'proprietary'
-        node = '130 nm'
+    case 4: #10 bit SkyWater at 32.07 MHz,
+        tech = 'SkyWater'
+        # node = '130 nm'
         method0 = 'static'
-        method1 = 'Spectre'
+        method1 = 'spice'
 
         methods = f'{method0}-{method1}'
 
-        Nb = 16
-        Fs = 65.47
+        Nb = 10
+        Fs = 32.07
+
         static_baseline = 7.00
         static_physcal = 12.61
         static_nsdcal = 18.65
         static_phfd = 9.93
         static_shpd = 7.80
         static_dem = 9.22
-
-        mos_model_baseline = 5.98
-        spectre_baseline = mos_model_baseline
-        spectre_physcal = 5.86
-        spectre_nsdcal = 5.90
-        spectre_phfd = 4.66
-        spectre_shpd = 5.22
-        spectre_dem = 6.25
-        # spice_ilc_16_1= 14.646
-        # spice_mpc_16_1= 14.646
-
-        # Calculate gains
-        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem]) - static_baseline
-        spectre_gains = np.array([spectre_physcal, spectre_nsdcal, spectre_phfd, spectre_shpd, spectre_dem]) - mos_model_baseline
-        mos_model_gains = spectre_gains
-
-        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM']
-    
-    case 5: # 16-bit DAC 2 Ch from Trond 33 MHz
-        tech = 'SkyWater'
-        node = 'SKY130'
-        method0 = 'static'
-        method1 = 'SPICE'
-
-        methods = f'{method0}-{method1}'
-
-        Nb = 16
-        Fs = 32.7
-
-        static_baseline = 11.514
-        static_physcal = 16.940
-        static_nsdcal = 18.332
-        static_phfd = 15.886
-        static_shpd= 11.303
-        static_dem = 11.141 
-        
+        static_mhoq= 15.551
 
         # Spice simulation results
-        mos_model_baseline = 11.134
+        mos_model_baseline = 6.093
         spice_baseline = mos_model_baseline
-        spice_physcal = 16.913
-        spice_nsdcal = 16.986
-        spice_phfd = 15.109
-        spice_shpd= 11.172
-        spice_dem = 11.184
-        
+        spice_physcal = 6.164
+        spice_nsdcal = 8.343
+        spice_phfd = 6.783
+        spice_shpd= 3.522
+        spice_dem = 4.874
+        spice_mhoq = 1
         # Calculate gains
-        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem]) - static_baseline
-        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem]) - mos_model_baseline
+        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem, static_mhoq]) - static_baseline
+        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem, spice_mhoq]) - mos_model_baseline
         mos_model_gains = spice_gains
 
-        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM']
+        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM', 'MHOQ']
     
-    case 6: # 6-bit DAC 2 Ch from Trond 33 MHz
-        tech = 'SkyWater'
-        node = 'SKY130'
+    case 5: ## 6 bit ZTC ARTI
+        tech = 'ZTC ARTI (Proprietary)'
+        # node = 'SKY130'
         method0 = 'static'
-        method1 = 'SPICE'
+        method1 = 'spectre'
 
         methods = f'{method0}-{method1}'
 
         Nb = 6
-        Fs = 32.7
+        Fs = 209.72 
 
-        static_baseline = 6.389
-        static_physcal = 8.206
-        static_nsdcal = 16.476
-        static_phfd = 8.828
-        static_shpd= 7.461
-        static_dem = 7.393
+        static_baseline = 11.00
+        static_physcal = 10.93
+        static_nsdcal = 11.06
+        static_phfd = 10.40
+        static_shpd= 10.82
+        static_dem = 10.92
+        static_mhoq= 12.44
+        
 
-        # Spice simulation results
-        mos_model_baseline = 6.379
-        spice_baseline = mos_model_baseline
-        spice_physcal = 6.483
-        spice_nsdcal = 11.21
-        spice_phfd = 8.85
-        spice_shpd = 7.502
-        spice_dem = 7.401
+        # Spectre simulation results
+        mos_model_baseline = 10.22
+        spectre_baseline = mos_model_baseline
+        spectre_physcal = 9.90
+        spectre_nsdcal = 9.80
+        spectre_phfd = 10.36
+        spectre_shpd = 9.96
+        spectre_dem = 9.69
+        spectre_mhoq = 9.67
+        
+        # Calculate gains
+        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem, static_mhoq]) - static_baseline
+        spectre_gains = np.array([spectre_physcal, spectre_nsdcal, spectre_phfd, spectre_shpd, spectre_dem, spectre_mhoq]) - mos_model_baseline
+        mos_model_gains = spectre_gains
+
+        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM','MHOQ']
+    
+    case 6: # 10 bit ZTC ARTI
+        tech = 'ZTC ARTI (Proprietary)'
+        # node = 'SKY130'
+        method0 = 'static'
+        method1 = 'sprectre'
+
+        methods = f'{method0}-{method1}'
+
+        Nb = 10
+        Fs = 209.72 
+
+        static_baseline = 10.99 
+        static_physcal = 14.96 
+        static_nsdcal = 18.78 
+        static_phfd = 14.35 
+        static_shpd= 11.66 
+        static_dem = 12.32 
+        static_mhoq= 9.99 
+
+
+        # Spectre simulation results
+        mos_model_baseline = 8.92
+        spectre_baseline = mos_model_baseline
+        spectre_physcal = 8.66
+        spectre_nsdcal = 8.50
+        spectre_phfd = 9.43
+        spectre_shpd = 8.92
+        spectre_dem = 8.05
+        spectre_mhoq = 9.23
+        
 
         # Calculate gains
-        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem]) - static_baseline
-        spice_gains = np.array([spice_physcal, spice_nsdcal,  spice_phfd, spice_shpd, spice_dem]) - mos_model_baseline
-        mos_model_gains = spice_gains
+        static_gains = np.array([static_physcal, static_nsdcal, static_phfd, static_shpd, static_dem, static_mhoq]) - static_baseline
+        spectre_gains = np.array([spectre_physcal, spectre_nsdcal, spectre_phfd, spectre_shpd, spectre_dem, spectre_mhoq]) - mos_model_baseline
+        mos_model_gains = spectre_gains
 
-        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM']
+        lin_methods =  ['PHYSCAL', 'NSDCAL', 'PHFD', 'SHPD', 'DEM','MHOQ']
 
 # %% Plots
 barWidth = 0.25
@@ -280,7 +287,9 @@ for rect in b1 + b2 :
 #     if height < 0 :
 #         plt.text(rect.get_x() + rect.get_width() / 2.0 -0.03, 0.5, '1 MHz', rotation = 90, fontsize = 13)        
 
-plt.title(f"{int(Nb)}-bit DAC | Technology: {tech} {node} | Fs: {Fs} MHz\n{method0} baseline: {static_baseline} bits | {method1} baseline: {mos_model_baseline} bits", fontsize = "13")
+plt.title(f"{int(Nb)}-bit DAC | Technology: {tech} | Fs: {Fs} MHz", fontsize = "13")
+
+# plt.title(f"{int(Nb)}-bit DAC | Technology: {tech} {node} | Fs: {Fs} MHz\n{method0} baseline: {static_baseline} bits | {method1} baseline: {mos_model_baseline} bits", fontsize = "13")
 plt.legend(fontsize="13", loc='upper right')
 ax.set_axisbelow(True)
 ax.grid(zorder=0, axis = "y")
@@ -288,7 +297,27 @@ fig.tight_layout()
 # plt.savefig(f"Gainplot-{Nb}bits.pdf")
 
 # %%
-fname = f"figures/Gainplot_{Nb}b_{tech}_{node}_{int(Fs)}MHz_{methods}".replace(" ", "_")
+# fname = f"figures/Gainplot_{Nb}b_{tech}_{node}_{int(Fs)}MHz_{methods}".replace(" ", "_")
+# fname = str(fname)
+# fig.savefig(fname + ".svg", format='svg', bbox_inches='tight') # Practical for PowerPoint and other applications
+# fig.savefig(fname + ".pdf", format='pdf', bbox_inches='tight') # Best for LaTeX
+
+# %%
+
+# fname = f"Gainplot_{Nb}b_{tech}_{node}_{int(Fs)}MHz_{methods}".replace(" ", "_")
+fname = f"Gainplot_{Nb}b_{tech}_{int(Fs)}MHz_{methods}".replace(" ", "_")
 fname = str(fname)
-fig.savefig(fname + ".svg", format='svg', bbox_inches='tight') # Practical for PowerPoint and other applications
-fig.savefig(fname + ".pdf", format='pdf', bbox_inches='tight') # Best for LaTeX
+
+script_dir = os.path.dirname(__file__)
+results_dir = os.path.join(script_dir, 'Figures/')
+if not os.path.isdir(results_dir):
+    os.makedirs(results_dir)
+
+# plt.plot([1,2,3,4])
+# plt.ylabel('some numbers')
+plt.savefig(results_dir + fname, format ='svg', bbox_inches ='tight')
+plt.savefig(results_dir + fname, format ='pdf', bbox_inches ='tight')
+
+# fig.savefig(fname + ".svg", format='svg', bbox_inches='tight') # Practical for PowerPoint and other applications
+# fig.savefig(fname + ".pdf", format='pdf', bbox_inches='tight') # Best for LaTeX
+# %%
